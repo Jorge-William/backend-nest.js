922fb6241c05003f8bc047086469ad024d0b2a41
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-09-04 16:03:54 -0300

    Feat: inplementing fake database

---
+++ database/fakeDatabase.ts
@@ -0,0 +1,17 @@
+export const db = [
+  {
+    id: 'd4dc1dd3-1ff1-410c-b2d8-76ec6dc8b836',
+    title: 'Fazer compras',
+    completed: false,
+  },
+  {
+    id: '20769082-6468-4e35-99a1-789042a1aadd',
+    title: 'Estudar Nest.js',
+    completed: false,
+  },
+  {
+    id: 'f93b4b81-b41a-4f22-b9e8-e8f101ddde1e',
+    title: 'Ler um livro',
+    completed: true,
+  },
+];
