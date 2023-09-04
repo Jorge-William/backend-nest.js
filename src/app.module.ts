7900a942433e9310bc6af6f8c47224f454c9e1af
Jorge William <jorge_william_cardoso@yahoo.com.br>
2023-09-04 12:26:07 -0300

    Fix: Registering Controller and Provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TaskService } from './task/task.service';
+import { TaskController } from './task/task.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [TaskController],
+  providers: [TaskService],
 })
 export class AppModule {}
